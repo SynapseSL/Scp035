99dc67233b7f3ae6e00fc9aa19fdfc85f1bd0ad1
GrafDimenzio <crafterwolfhd@gmail.com>
2020-11-14 16:41:06 +0100

    added Config Description

--- Scp035/Config.cs
+++ Scp035/Config.cs
@@ -1,22 +1,30 @@
 ﻿using Synapse.Config;
+using System.ComponentModel;
 using System.Collections.Generic;
 
 namespace Scp035
 {
     public class Config : AbstractConfigSection
     {
+        [Description("The Health of Scp035")]
         public int Scp035Health = 150;
 
+        [Description("The Badge of Scp035")]
         public string BadgeName = "Scp-035";
 
+        [Description("The Color which the Badge of Scp035 should have")]
         public string BadgeColor = "red";
 
+        [Description("If the Spectator that are the longest time death should become 035")]
         public bool DeathTime = true;
 
+        [Description("The max amount of Scp035 items that can exist")]
         public int Scp035ItemsAmount = 3;
 
+        [Description("The Items that the Scp035 mask can be")]
         public List<int> Possible035Items = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 24, 25, 26, 27, 30, 31, 32, 33, 34 };
 
+        [Description("The interval in which the mask should change its position")]
         public float PickupSpawnInterval = 30f;
     }
 }
